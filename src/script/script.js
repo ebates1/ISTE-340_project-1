5c7b716903206981b2f58d07f04d0e908bb67dfd
elibates1 <emb2065@rit.edu>
2021-02-04 21:02:55 +0100

    huge layotu changes

---
+++ src/script/script.js
@@ -0,0 +1,20 @@
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+AOS.init();
+
+const headerButton = document.querySelector('.landing__linkbutton');
+
+document.querySelector('.nav__list').addEventListener('click', function (e) {
+  e.preventDefault();
+  const target = e.target;
+  if (target.classList.contains('nav-list__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+headerButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  document.querySelector('#services').scrollIntoView({ behavior: 'smooth' });
+});
